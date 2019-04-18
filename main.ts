0ce9ea5417ed45c71bee1bac0abba3ca1788e6d5
Konstantin Gassmann <kg@creemedia.com>
2019-04-18 12:35:59 +0200

    new shader

--- main.ts
+++ main.ts
@@ -1,151 +1,160 @@
-import vert from './shader.vert'
-import frag from './shader.frag'
-import { rect, createShader, createProgram, getAttribs, getUniforms, drawImage, createTexture, createTexCoordBuffer } from '../utils'
-import { debounce } from 'lodash'
-import tex from './tex.jpg'
-import lerp from 'lerp'
+import vert from "./shader.vert";
+import frag from "./shader.frag";
 
-const canvas = document.getElementById('paper') as HTMLCanvasElement
-const gl = canvas.getContext('webgl')
+import {
+  rect,
+  createShader,
+  createProgram,
+  getAttribs,
+  getUniforms,
+  drawImage,
+  createTexture,
+  createTexCoordBuffer,
+  createBuffer
+} from "../utils";
+import { debounce } from "lodash";
+import tex from "./tex.jpg";
+import lerp from "lerp";
 
-const textCanvas = document.getElementById('text') as HTMLCanvasElement
-const ctx = textCanvas.getContext('2d')
+const canvas = document.getElementById("paper") as HTMLCanvasElement;
+const gl = canvas.getContext("webgl");
 
-const cursorCanvas = document.getElementById('cursor') as HTMLCanvasElement
-const cursorCtx = cursorCanvas.getContext('2d')
+const textCanvas = document.getElementById("text") as HTMLCanvasElement;
+const ctx = textCanvas.getContext("2d");
 
-const size = Math.min(window.innerWidth, window.innerHeight)
+const cursorCanvas = document.getElementById("cursor") as HTMLCanvasElement;
+const cursorCtx = cursorCanvas.getContext("2d");
 
-let texWidth = size * 0.75
-let texHeight = size * 0.75
-const image = new Image()
-let textures = []
+const size = Math.min(window.innerWidth, window.innerHeight);
+
+let texWidth = size * 0.75;
+let texHeight = size * 0.75;
+const image = new Image();
+let textures = [];
 
 const setCanvasSize = () => {
-  const size = Math.min(window.innerWidth, window.innerHeight)
+  const size = Math.min(window.innerWidth, window.innerHeight);
 
-  texWidth = size * 0.75
-  texHeight = size * 0.75
+  texWidth = size * 0.75;
+  texHeight = size * 0.75;
 
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-  textCanvas.width = texWidth
-  textCanvas.height = texHeight
+  textCanvas.width = texWidth;
+  textCanvas.height = texHeight;
 
-  cursorCanvas.width = window.innerWidth
-  cursorCanvas.height = window.innerHeight
-}
+  cursorCanvas.width = window.innerWidth;
+  cursorCanvas.height = window.innerHeight;
+};
 
-window.addEventListener('resize', debounce(() => {
-  setCanvasSize()
-  
-  ctx.clearRect(0, 0, textCanvas.width, textCanvas.height)
-  ctx.fillText('K', textCanvas.width / 2, textCanvas.height / 2)
-}, 100))
+window.addEventListener(
+  "resize",
+  debounce(() => {
+    setCanvasSize();
 
-let mouseX = 0
-let mouseY = 0
-let velocityX = 0
-let velocityY = 0
+    ctx.clearRect(0, 0, textCanvas.width, textCanvas.height);
+    ctx.fillText("K", textCanvas.width / 2, textCanvas.height / 2);
+  }, 100)
+);
 
-window.addEventListener('mousemove', (e) => {
-  mouseX = e.clientX
-  mouseY = e.clientY
-})
+let mouseX = 0;
+let mouseY = 0;
+let velocityX = 0;
+let velocityY = 0;
 
-document.addEventListener('mouseenter', (e) => {
-  mouseX = e.clientX
-  mouseY = e.clientY
-})
+window.addEventListener("mousemove", e => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
 
-setCanvasSize()
+document.addEventListener("mouseenter", e => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
 
-ctx.beginPath()
-ctx.textAlign = 'left'
-ctx.textBaseline = 'middle'
-ctx.textAlign = 'center'
-ctx.strokeStyle = '#3434ff'
-ctx.lineWidth = 4
-ctx.font = `${ texHeight }px sans-serif`;
-ctx.strokeText('K', textCanvas.width / 2, textCanvas.height / 2)
+setCanvasSize();
 
-const vertexShader = createShader(gl, gl.VERTEX_SHADER, vert)
-const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, frag)
+ctx.beginPath();
+ctx.textAlign = "left";
+ctx.textBaseline = "middle";
+ctx.textAlign = "center";
+ctx.strokeStyle = "#3434ff";
+ctx.lineWidth = 4;
+ctx.font = `${texHeight}px sans-serif`;
+ctx.strokeText("K", textCanvas.width / 2, textCanvas.height / 2);
 
-const program = createProgram(gl, vertexShader, fragmentShader)
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vert);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, frag);
 
-const attributes = getAttribs(gl, program, ['position', 'texCoord'])
-const uniforms = getUniforms(gl, program, ['matrix', 'image', 'letter', 'velocity'])
+const program = createProgram(gl, vertexShader, fragmentShader);
 
-const imageTexCoordBuffer = createTexCoordBuffer(gl, attributes.texCoord, 8, 8)
-const letterTexCoordBuffer = createTexCoordBuffer(gl, attributes.texCoord)
+const attributes = getAttribs(gl, program, ["position", "texCoord"]);
+const uniforms = getUniforms(gl, program, [
+  "matrix",
+  "image",
+  "letter",
+  "velocity",
+  "image_matrix"
+]);
 
-const imgBuffer = gl.createBuffer()
-const texbuffer = gl.createBuffer()
+const [imgBuffer, imageQuadLength] = createBuffer(gl, 8, 8);
+const [letterBuffer] = createBuffer(gl);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, imgBuffer)
+gl.disable(gl.DEPTH_TEST);
+gl.enable(gl.BLEND);
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-const imageQuadLength = rect({
-  gl,
-  x: 0,
-  y: 0,
-  width: 1,
-  height: 1,
-  subX: 8,
-  subY: 8
-})
-
-gl.bindBuffer(gl.ARRAY_BUFFER, texbuffer)
-
-rect({
-  gl,
-  x: 0,
-  y: 0,
-  width: 1,
-  height: 1
-})
-
-
-gl.disable(gl.DEPTH_TEST)
-gl.enable(gl.BLEND)
-gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-
-let backgroundX = 0
-let backgroundY = 0
-let lastMouseX = 0
-let lastMouseY = 0
+let backgroundX = 0;
+let backgroundY = 0;
+let lastMouseX = 0;
+let lastMouseY = 0;
+let mouseScale = 0;
+let currentMouseScale = 1;
 
 const draw = () => {
-  requestAnimationFrame(draw)
-  velocityX = lerp(velocityX, lastMouseX - mouseX, 0.1)
-  velocityY = lerp(velocityY, lastMouseY - mouseY, 0.1)
+  requestAnimationFrame(draw);
 
-  backgroundX = lerp(backgroundX, mouseX, 0.1)
-  backgroundY = lerp(backgroundY, mouseY, 0.1)
+  if (
+    mouseX < 10 ||
+    mouseY < 10 ||
+    mouseX > window.innerWidth - 10 ||
+    mouseY > window.innerHeight - 10
+  ) {
+    currentMouseScale = 0;
+  } else {
+    currentMouseScale = 1;
+  }
 
-  lastMouseX = mouseX
-  lastMouseY = mouseY
+  velocityX = lerp(velocityX, lastMouseX - mouseX, 0.1);
+  velocityY = lerp(velocityY, lastMouseY - mouseY, 0.1);
+  mouseScale = lerp(mouseScale, currentMouseScale, 0.5);
 
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+  backgroundX = lerp(backgroundX, mouseX, 0.1);
+  backgroundY = lerp(backgroundY, mouseY, 0.1);
 
-  gl.clearColor(0, 0, 0, 0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  lastMouseX = mouseX;
+  lastMouseY = mouseY;
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
   // draw the mouse circle
-  cursorCtx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-  cursorCtx.beginPath()
-  cursorCtx.strokeStyle = '#3434ff'
-  cursorCtx.lineWidth = 2
-  cursorCtx.arc(mouseX, mouseY, 10, 0, Math.PI * 2)
-  cursorCtx.stroke()
+  cursorCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  cursorCtx.beginPath();
+  cursorCtx.strokeStyle = "#3434ff";
+  cursorCtx.lineWidth = 2;
+  cursorCtx.arc(mouseX, mouseY, 10 * mouseScale, 0, Math.PI * 2);
+  cursorCtx.stroke();
 
-  gl.useProgram(program)
+  gl.useProgram(program);
 
   // draw the image
-  gl.bindTexture(gl.TEXTURE_2D, textures[0])
-  gl.uniform2fv(uniforms.velocity, [velocityX, velocityY])
-  
+  gl.bindTexture(gl.TEXTURE_2D, textures[0]);
+  gl.uniform2fv(uniforms.velocity, [velocityX, velocityY]);
+
   drawImage({
     gl,
     x: backgroundX - image.width / 2,
@@ -153,50 +162,47 @@ const draw = () => {
     w: image.width,
     h: image.height,
     positionBuffer: imgBuffer,
-    texcoordBuffer: imageTexCoordBuffer,
+    texcoordBuffer: imgBuffer,
     matrixLocation: uniforms.matrix,
     textureLocation: uniforms.image,
     texLocation: attributes.texCoord,
     positionLocation: attributes.position
-  })
+  });
 
-  gl.drawArrays(gl.TRIANGLES, 0, imageQuadLength / 2)
+  gl.drawArrays(gl.TRIANGLES, 0, imageQuadLength / 2);
 
   // draw the letter K
-  gl.bindTexture(gl.TEXTURE_2D, textures[1])
-  gl.uniform2fv(uniforms.velocity, [0, 0])
+  gl.bindTexture(gl.TEXTURE_2D, textures[1]);
+  gl.uniform2fv(uniforms.velocity, [0, 0]);
 
   drawImage({
     gl,
     x: gl.canvas.width / 2 - texWidth / 2,
     y: gl.canvas.height / 2 - texHeight / 2,
-    w: texWidth,
-    h: texHeight,
-    positionBuffer: texbuffer,
-    texcoordBuffer: letterTexCoordBuffer,
+    w: texWidth * 0.5,
+    h: texHeight * 0.5,
+    positionBuffer: letterBuffer,
+    texcoordBuffer: letterBuffer,
     matrixLocation: uniforms.matrix,
     textureLocation: uniforms.image,
     texLocation: attributes.texCoord,
     positionLocation: attributes.position
-  })
+  });
 
-  gl.drawArrays(gl.TRIANGLES, 0, 6)
-}
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+};
 
 image.onload = () => {
-  mouseX = window.innerWidth / 2 + image.width / 2
-  mouseY = window.innerHeight / 2
-  
-  lastMouseX = mouseX
-  lastMouseY = mouseY
-  
-  backgroundX = mouseX
-  backgroundY = mouseY
+  mouseX = window.innerWidth / 2 + image.width / 2;
+  mouseY = window.innerHeight / 2;
 
-  textures = [
-    createTexture(gl, image),
-    createTexture(gl, ctx.canvas)
-  ]
-  requestAnimationFrame(draw)
-}
-image.src = tex
+  lastMouseX = mouseX;
+  lastMouseY = mouseY;
+
+  backgroundX = mouseX;
+  backgroundY = mouseY;
+
+  textures = [createTexture(gl, image), createTexture(gl, ctx.canvas)];
+  requestAnimationFrame(draw);
+};
+image.src = tex;
--- shader.frag
+++ shader.frag
@@ -4,6 +4,6 @@ uniform sampler2D u_image;
 
 varying vec2 v_texCoord;
 
-void main () {
-  gl_FragColor = texture2D(u_image, v_texCoord);
+void main(){
+  gl_FragColor=texture2D(u_image,v_texCoord);
 }
